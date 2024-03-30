7ec57c1feba870c51ebd2b50e395eeebf13379af
Fernando Canitrot <canitrot.fernando@gmail.com>
2024-03-30 14:10:08 -0300

    Add animation fot js

---
+++ js/animaciones.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.header__index', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-180px',
+});
+
+sr.reveal('.main__section', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-180px',
+});
